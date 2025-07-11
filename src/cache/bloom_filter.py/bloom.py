160b91cd7f6d3d9ec57558cbc4e6124add18d6ec
Simon Stensgaard <STcena1@hotmail.com>
2025-07-11 13:00:35 +0200

    Started working on Codebase - Added global cache and Bloom Filter

--- src/cache/bloom_filter.py/bloom.py
+++ src/cache/bloom_filter.py/bloom.py
@@ -9,7 +9,27 @@ from bitarray import bitarray
 
 class BloomFilter(object):
     def __init__(self, items_count, probability):
-        pass
+        self.probability = probability
+        self.size = self.get_size(items_count, probability)
+        self.hash_count = self.get_hash_count(self.size, items_count)
+        self.bit_array = bitarray(self.size)
+        self.bit_array.setall(0)
+
+    def add(self, item):
+        digests = []
+        for i in range(self.hash_count):
+            digest = hash(item, i) % self.size
+            digests.append(digest)
+
+            self.bit_array[digest] = True
+
+    def check(self, item):
+        for i in range(self.hash_count):
+            digest = hash(item, i) % self.size
+            if self.bit_array[digest] == False:
+
+                return False
+        return True
 
     @classmethod
     def get_size(self, n, p):
@@ -17,6 +37,6 @@ class BloomFilter(object):
         return int(m)
     
     @classmethod
-    def hash_count(self, m, n):
+    def get_hash_count(self, m, n):
         k = (m/n) * math.log(2)
         return int(k)
