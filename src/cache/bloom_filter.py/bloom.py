d084ce791e1e0439011dadf58ef41bb95b0439b6
Simon Stensgaard <STcena1@hotmail.com>
2025-07-11 17:25:33 +0200

    Changed Bloomfilter class to use staticmethods and mmh3 lib for hashing

--- src/cache/bloom_filter.py/bloom.py
+++ src/cache/bloom_filter.py/bloom.py
@@ -1,7 +1,7 @@
 # --------------- Imports ---------------
 
 import math
-import hashlib
+import mmh3
 
 from bitarray import bitarray
 
@@ -16,27 +16,28 @@ class BloomFilter(object):
         self.bit_array.setall(0)
 
     def add(self, item):
-        digests = []
         for i in range(self.hash_count):
-            digest = hash(item, i) % self.size
-            digests.append(digest)
+            digest = self._hash(item, i) % self.size
 
             self.bit_array[digest] = True
 
     def check(self, item):
         for i in range(self.hash_count):
-            digest = hash(item, i) % self.size
+            digest = self._hash(item, i) % self.size
             if self.bit_array[digest] == False:
 
                 return False
         return True
+    
+    def _hash(self, item, seed):
+        return mmh3.hash(item, seed)
 
-    @classmethod
-    def get_size(self, n, p):
+    @staticmethod
+    def get_size(n, p):
         m = -(n * math.log(p)) / (math.log(2) ** 2)
         return int(m)
     
-    @classmethod
-    def get_hash_count(self, m, n):
+    @staticmethod
+    def get_hash_count(m, n):
         k = (m/n) * math.log(2)
         return int(k)
